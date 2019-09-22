d6a3d2325adad9b6c9fcc8851bdfd4d0cf3b5d34
Rinita Nair <rnair3@uncc.edu>
2019-09-22 17:10:28 -0400

    Adding new Scenes and apk file

---
+++ ARGreetingCard/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void GreetingCard()
+    {
+        SceneManager.LoadScene("GreetingCard");
+    }
+
+    public void RotateCube()
+    {
+        SceneManager.LoadScene("Rotate Cube");
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
